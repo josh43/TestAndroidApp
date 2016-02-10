020359f7b34f81d5682beed6dddffec8df190552
joshua <josh@joshuas-iMac.local>
2016-02-10 15:51:35 -0800

    First push

---
+++ app/src/main/java/com/example/josh/basicgame/NewCppClass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by joshua on 2/10/16.
+//
+
+#include "NewCppClass.h"
---
+++ app/src/main/res/res/java/NewCppClass.h
@@ -0,0 +1,16 @@
+//
+// Created by joshua on 2/10/16.
+//
+
+#ifndef CHAPTER4_NEWCPPCLASS_H
+#define CHAPTER4_NEWCPPCLASS_H
+
+
+
+class NewCppClass {
+
+};
+
+
+
+#endif //CHAPTER4_NEWCPPCLASS_H
